4439710eae82b76e4bb87fb5c2419b7fd9743b2e
Chaos Leong <lgf42031@gmail.com>
2017-03-14 23:21:12 +0800

    Initial commit

---
+++ app/src/main/java/org/chaos/fx/manga/MainActivity.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2017 Chaos Leong
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.chaos.fx.manga
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private var mTextMessage: TextView? = null
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                mTextMessage!!.setText(R.string.title_home)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_favorite -> {
+                mTextMessage!!.setText(R.string.title_favorite)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_settings -> {
+                mTextMessage!!.setText(R.string.title_settings)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mTextMessage = findViewById(R.id.message) as TextView
+        val navigation = findViewById(R.id.navigation) as BottomNavigationView
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+}
